191bf35f4bf1340f29a7a4e6185d0d9bedcc5de1
JamesKliblurn <james.kilburn062@students.cmcss.net>
2025-12-12 07:59:33 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -9,7 +9,7 @@ milestone_increment = 1000
 # Calculate follower statistics and milestone progress
 
 # Calculate milestone progress
-current_milestone = current_followers / milestone_increment
+current_milestone = current_followers // milestone_increment
 progress_in_milestone = current_followers % milestone_increment
 # Calculate growth statistics
 total_gained = current_followers - starting_followers
